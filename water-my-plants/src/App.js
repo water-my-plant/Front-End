8f9b329e5863a3bf38ad5805a4b1a618974ea8e5
Kyle Leonard <kdleonard@icloud.com>
2019-09-18 20:36:25 -0500

    Imported formik to add a new plant and added axios to post.
    Got an error so will need to fix that but got a good start

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/Home";
+import NewPlant from "./components/Plants";
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Route path="/home" component={Home} />
+      <Route path="/plants" component={NewPlant} />
     </div>
   );
 }
--- water-my-plants/src/components/Plants.js
+++ water-my-plants/src/components/Plants.js
@@ -2,11 +2,51 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import axios from "axios";
+import { withFormik, Form, Field } from "formik";
+import * as yup from "yup";
 
-const NewPlant = props => {
+const NewPlant = ({ errors, touched, status }) => {
   const [newPlant, addNewPlant] = useState([]);
 
-  return "";
+  return (
+    <Form>
+      {touched.plant && errors.plant && <p className="error">{errors.plant}</p>}
+      <Field type="text" name="plant" placeholder="Plant Name" />
+
+      {touched.species && errors.species && (
+        <p className="error">{errors.species}</p>
+      )}
+      <Field type="text" name="species" placeholder="Species" />
+
+      {touched.water && errors.water && <p className="error">{errors.water}</p>}
+      <Field type="text" name="water" placeholder="Water Schedule" />
+
+      <button>Submit!</button>
+    </Form>
+  );
 };
 
-export default NewPlant;
+export default withFormik({
+  mapPropsToValues: values => {
+    return {
+      plant: values.plant || "",
+      species: values.species || "",
+      water: values.water || ""
+    };
+  },
+  validationSchema: yup.object().shape({
+    plant: yup.string().required("Add plant name"),
+    species: yup.string().required("What's it's species?"),
+    water: yup.string().required("Diet is required")
+  }),
+  handleSubmit: (values, { setStatus }) => {
+    axios
+      .post("", values)
+      .then(response => {
+        setStatus(response.data);
+      })
+      .catch(error => {
+        console.log("Error:", error);
+      });
+  }
+})(NewPlant);
