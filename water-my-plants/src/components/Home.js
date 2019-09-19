4117dae404a88800058ad95ab3a929efb7ba8112
Ruth Poliakon <r.poliakon@gmail.com>
2019-09-18 18:00:35 -0600

    installed react router, wrapped app

--- water-my-plants/src/components/Home.js
+++ water-my-plants/src/components/Home.js
@@ -1,1 +1,27 @@
+import React from "react";
+// import axios from "axios";
+
 // list of plants
+
+export default function Home() {
+  // const [data, setData] = useState([]);
+
+  // get plant data from database
+  //   useEffect(() => {
+  //     axios
+  //       .get(``)
+  //       .then(response => {
+  //         console.log(response.data.results);
+  //         // setData(response.data.results);
+  //       })
+  //       .catch(error => {
+  //         console.log(error);
+  //       });
+  //   }, []);
+
+  return (
+    <div>
+      <h1>My Plants</h1>
+    </div>
+  );
+}
