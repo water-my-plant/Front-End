9ed12656fad3cd4c517ecd88e9ddd3e0640caefe
Ian <mus5002@aol.com>
2019-09-19 21:34:27 -0400

    changed component name and path for router, more styling, started on login component

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/Home";
-import UserForm from "./components/Login";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/form" component={UserForm} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/login" component={Login} />
       <Route path="/home" component={Home} />
     </div>
   );
--- water-my-plants/src/components/SignUp.js
+++ water-my-plants/src/components/SignUp.js
@@ -51,34 +51,35 @@ const Error = styled.p`
     margin-top: -.12rem;
     z-index: 3;
 `
-const Pot = styled.div`
-    position: absolute;
-    width: 70px;
-    height: 0;
-    border-left: 12px solid transparent;
-    border-right: 12px solid transparent;        
-    border-top: 60px solid #FF7043;
-    bottom: 5rem;
-    left: 20rem;
-    z-index: 2;
-`
 
-const Login = (props) => {
+const SignUp = (props) => {
     const { errors, touched, status, values } = props
-
+    console.log('submitted ', values)
     return (
         <FormDiv>
-            <Heading>Login to Water My Plants</Heading>
+            <Heading>Sign Up for Water My Plants</Heading>
+
+            {touched.fullname && errors.fullname && <Error>{errors.fullname}</Error>}
+            <Input type='text' name='fullname' placeholder='fullname' />
 
             {touched.username && errors.username && <Error>{errors.username}</Error>}
             <Input type='text' name='username' placeholder='username' />
 
+            {touched.phoneNumber && errors.phoneNumber && <Error>{errors.phoneNumber}</Error>}
+            <Input type='text' name='phoneNumber' placeholder='phone number' />
+
             {touched.password && errors.password && <Error>{errors.password}</Error>}
             <Input type='text' name='password' placeholder='password' />
 
-            <Button type='submit'>Login</Button>
+            {touched.password2 && errors.password2 && <Error>{errors.password2.slice(49, 69)}</Error>}
+            <Input type='text' name='password2' placeholder='confirm password' />
 
-            <Pot></Pot>
+            {touched.termsOfService && errors.termsOfService && <Error>{errors.termsOfService.slice(58, 96)}</Error>}
+            <Label>
+                <Field type='checkbox' name='termsOfService' checked={values.termsOfService} />
+                <span>Terms of Service</span>
+            </Label>
+            <Button type='submit'>Sign Up</Button>
         </FormDiv>
     )
 }
@@ -86,13 +87,21 @@ const Login = (props) => {
 export default withFormik({    
     mapPropsToValues: (values) => {
         return {
+            fullname: values.fullname || '',
             username: values.username || '',
             password: values.password || '',
+            password2: values.password2 || '',
+            phoneNumber: values.phoneNumber || '',
+            termsOfService: values.termsOfService || false
         }
     },
     validationSchema: yup.object().shape({
-        username: yup.string().required(),
-        password: yup.string().required(),
+        fullname: yup.string().max(40, 'enter no more than 40 characters').required(),
+        username: yup.string().min(5, 'your username must have at least 5 characters').required(),
+        password: yup.string().min(8, 'password must be at least 8 characters').required('enter and confirm password'),
+        password2: yup.string().oneOf([yup.ref('password'), null, 'passwords must match']).required(),
+        phoneNumber: yup.number().positive().required(),
+        termsOfService: yup.boolean().oneOf([true, 'you must agree to the terms of service']).required()
     }),
     validateOnChange: false,
     validateOnBlur: false,
@@ -100,4 +109,4 @@ export default withFormik({
         console.log(values)
         return resetForm()
     }
-})(Login)
+})(SignUp)
