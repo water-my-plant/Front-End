0d167d7d9dc73dcfbdad46267d7af86f44eb0551
Kyle Leonard <kdleonard@icloud.com>
2019-09-19 19:33:33 -0500

    Added basic styling to the newPlant form
    Not displaying on screen. Need to debug

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import NewPlant from "./components/Plants";
-
 
 function App() {
   return (
     <div className="App">
       <Route path="/home" component={Home} />
-      <Route path="/plants" component={NewPlant} />
+      <Route path="/plants" component={NewPlant}></Route>
     </div>
   );
 }
