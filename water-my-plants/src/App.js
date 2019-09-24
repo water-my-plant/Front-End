9fd91c2aa7f090983f1ef1737bcc8378fd38cbcd
Kyle Leonard <kdleonard@icloud.com>
2019-09-23 19:31:26 -0500

    Created an UpdatePlant.js and styled it similar to Plant.js
    Need to get help on making sure I'm using the right axios requests to update data.

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import NewPlant from "./components/Plants";
+import UpdatePlant from "./components/UpdatePlant";
 
 function App() {
   return (
     <div className="App">
       <Route path="/home" component={Home} />
       <Route path="/plants" component={NewPlant}></Route>
+      <Route path="/updateplant" component={UpdatePlant}></Route>
     </div>
   );
 }
